507121c204c76fe98ba4f9379107a0ab3138e7ef
davide.antinori <antinoridavide@outlook.it>
2020-03-25 14:19:30 +0100

    replaced

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -9,34 +9,7 @@ console.log( calculator.mul() )
 console.log( calculator.div() )
 */
 
-const Calculator = function (values) {
-  return {
-    values: [...values],
-    add: function () {
-      return this.values.reduce((x, y) => x + y)
-    },
-    sub: function () {
-      return this.values.reduce((x, y) => x - y)
-    },
-    mul: function () {
-      return this.values.reduce((x, y) => x * y)
-    },
-    div: function () {
-      return (this.values.filter((x, i) => i > 0).includes(0) ? "Can't divide by zero." : this.values.reduce((x, y) => x / y))
-    }
-  }
-}
-
-let calculator = new Calculator([0, 1, 2, 3, 4])
-
-console.log(calculator.add())
-console.log(calculator.sub())
-console.log(calculator.mul())
-console.log(calculator.div())
-
-//--------------------------------------------------------------------------------
-
-function Calculator2(val) {
+function Calculator(val) {
   this.values = [...val]
   this.add = function () {
     return this.values.reduce((x, y) => x + y)
@@ -52,9 +25,9 @@ function Calculator2(val) {
   }
 }
 
-let calculator2 = new Calculator2([0, 1, 2, 3, 4])
+let calculator = new Calculator([0, 1, 2, 3, 4])
 
-console.log(calculator2.add())
-console.log(calculator2.sub())
-console.log(calculator2.mul())
-console.log(calculator2.div())
+console.log(calculator.add())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
